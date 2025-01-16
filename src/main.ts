d73fcc3687a9e76ce0cee5e9c36b532a2f04eb60
Sorry <5646769+sorry1@user.noreply.gitee.com>
2025-01-16 18:03:45 +0800

    fix: 修改demo

--- src/main.ts
+++ src/main.ts
@@ -16,113 +16,94 @@ const root = new Root({
   animationSwitch: true,
   children: [
     new Group({
-      width: 200,
-      justifyContent: "flex-end",
-      backgroundColor: "#d0cae3",
+      height: 80,
       children: [
-        new Element({
-          width: 50,
-          height: 50,
-          margin: [0, 0, 0, 0],
-          backgroundColor: "red"
+        new Group({
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#d0cae3",
+          width: 60,
+          children: [
+            new CircleImg({
+              src: "https://lf9-dp-fe-cms-tos.byteorg.com/obj/bit-cloud/VTable/custom-render/flower.jpg",
+              width: 50,
+              height: 50
+            })
+          ]
         }),
-        new Element({
-          width: 50,
-          height: 50,
-          margin: [0, 20, 0, 0],
-          backgroundColor: "red"
+        // 其他元素都是已知宽或高
+        // // 那么最后可以不用Expanded,会把剩余Group宽度高度拉满,Container会把宽度拉满
+        new Group({
+          flexDirection: "column",
+          children: [
+            new Expanded({
+              child: new Container({
+                padding: [0, 10, 0, 10],
+                backgroundColor: "red",
+                child: new Group({
+                  alignItems: "center",
+                  children: [
+                    new Text({
+                      font: {
+                        weight: "bold"
+                      },
+                      text: "hook哥还是牛的"
+                    })
+                  ]
+                })
+              })
+            }),
+            new Expanded({
+              child: new Container({
+                backgroundColor: "#c2d8cf",
+                padding: [0, 10, 0, 10],
+                child: new Group({
+                  alignItems: "center",
+                  children: [
+                    new Container({
+                      margin: [0, 10, 0, 0],
+                      padding: [0, 4, 0, 4],
+                      radius: 4,
+                      width: "auto",
+                      // rotate: 20,
+                      backgroundColor: "red",
+                      key: "俊飞盒子",
+                      child: new Text({
+                        color: "#fff",
+                        text: "hello",
+                        key: "俊飞"
+                      })
+                    }),
+                    new Container({
+                      margin: [0, 10, 0, 0],
+                      padding: [0, 4, 0, 4],
+                      radius: 4,
+                      width: "auto",
+                      backgroundColor: "red",
+                      child: new Text({
+                        color: "#fff",
+                        text: "俊飞"
+                      })
+                    }),
+                    new Container({
+                      margin: [0, 10, 0, 0],
+                      padding: [0, 4, 0, 4],
+                      radius: 4,
+                      width: "auto",
+                      backgroundColor: "red",
+                      child: new Text({
+                        color: "#fff",
+                        text: "hook"
+                      })
+                    })
+                  ]
+                })
+              })
+            })
+          ]
         })
       ]
     })
-    // new Group({
-    //   height: 80,
-    //   children: [
-    //     new Group({
-    //       justifyContent: "center",
-    //       alignItems: "center",
-    //       backgroundColor: "#d0cae3",
-    //       width: 60,
-    //       children: [
-    //         new CircleImg({
-    //           src: "https://lf9-dp-fe-cms-tos.byteorg.com/obj/bit-cloud/VTable/custom-render/flower.jpg",
-    //           width: 50,
-    //           height: 50
-    //         })
-    //       ]
-    //     }),
-    //     // 其他元素都是已知宽或高
-    //     // // 那么最后可以不用Expanded,会把剩余Group宽度高度拉满,Container会把宽度拉满
-    //     new Group({
-    //       flexDirection: "column",
-    //       children: [
-    //         new Expanded({
-    //           child: new Container({
-    //             padding: [0, 10, 0, 10],
-    //             backgroundColor: "red",
-    //             child: new Group({
-    //               alignItems: "center",
-    //               children: [
-    //                 new Text({
-    //                   font: {
-    //                     weight: "bold"
-    //                   },
-    //                   text: "hook哥还是牛的"
-    //                 })
-    //               ]
-    //             })
-    //           })
-    //         }),
-    //         new Expanded({
-    //           child: new Container({
-    //             backgroundColor: "#c2d8cf",
-    //             padding: [0, 10, 0, 10],
-    //             child: new Group({
-    //               alignItems: "center",
-    //               children: [
-    //                 new Container({
-    //                   margin: [0, 10, 0, 0],
-    //                   padding: [0, 4, 0, 4],
-    //                   radius: 4,
-    //                   width: "auto",
-    //                   // rotate: 20,
-    //                   backgroundColor: "red",
-    //                   key: "俊飞盒子",
-    //                   child: new Text({
-    //                     color: "#fff",
-    //                     text: "hello",
-    //                     key: "俊飞"
-    //                   })
-    //                 }),
-    //                 new Container({
-    //                   margin: [0, 10, 0, 0],
-    //                   padding: [0, 4, 0, 4],
-    //                   radius: 4,
-    //                   width: "auto",
-    //                   backgroundColor: "red",
-    //                   child: new Text({
-    //                     color: "#fff",
-    //                     text: "俊飞"
-    //                   })
-    //                 }),
-    //                 new Container({
-    //                   margin: [0, 10, 0, 0],
-    //                   padding: [0, 4, 0, 4],
-    //                   radius: 4,
-    //                   width: "auto",
-    //                   backgroundColor: "red",
-    //                   child: new Text({
-    //                     color: "#fff",
-    //                     text: "hook"
-    //                   })
-    //                 })
-    //               ]
-    //             })
-    //           })
-    //         })
-    //       ]
-    //     })
-    //   ]
-    // })
   ]
 });
 
