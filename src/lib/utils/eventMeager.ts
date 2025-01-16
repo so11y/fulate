38bf4d707b3245a0734a25e164e53567dca54b39
sorry <247076126@qq.com>
2025-01-16 23:10:53 +0800

    fix: todo

--- src/lib/utils/eventMeager.ts
+++ src/lib/utils/eventMeager.ts
@@ -47,6 +47,45 @@ export class EventManage extends EventTarget {
       parent.eventMeager.notify(eventName, event)
     }
   }
+  //还不知道是不是要改成setimeout来简单切一下
+  // notify(eventName: string, event?: CanvasPoint) {
+  //   const customEvent = new CustomEvent(eventName, {
+  //     detail: {
+  //       target: event?.target ?? this.target,
+  //       x: event?.x ?? 0,
+  //       y: event?.y ?? 0,
+  //       buttons: event?.buttons ?? 0,
+  //     },
+  //   });
+
+  //   if (this.hasUserEvent) {
+  //     this.target.dispatchEvent(customEvent);
+  //     if (customEvent.cancelBubble) {
+  //       return;
+  //     }
+  //   }
+
+  //   const notifyParent = (parent: Element | null) => {
+  //     if (!parent) {
+  //       return;
+  //     }
+
+  //     // 检查是否满足条件
+  //     if (
+  //       (parent.isInternal || parent.eventMeager.hasUserEvent === false) &&
+  //       parent !== this.target.root
+  //     ) {
+  //       // 继续向上查找父节点
+  //       setTimeout(() => notifyParent(parent.parent), 0);
+  //     } else {
+  //       // 找到符合条件的父节点，触发事件
+  //       parent.eventMeager.notify(eventName, event);
+  //     }
+  //   };
+
+  //   // 开始查找父节点
+  //   notifyParent(this.target.parent);
+  // }
 
   mounted() {
     this.addEventListener("click", this.target.click)
