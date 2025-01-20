4938ef86fdb7d963964f797ad55da879fd2d13d3
Sorry <5646769+sorry1@user.noreply.gitee.com>
2025-01-20 14:01:54 +0800

    feat: 移出lent

--- lent.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "lent";
-
-export default defineConfig({
-  port: 3099,
-  root: "./src",
-});
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  root: "./src"
-});
+export default defineConfig({});
